278d249c789a69a8fa8d1439c07a289dddf08bd3
Pedro Rosa <pedro_sxbr@usp.br>
2022-06-10 23:22:54 -0300

    minor changes

--- main.py
+++ main.py
@@ -28,8 +28,56 @@ def eq(key, dr, dtheta, r, k1, k2):
                         [0,dtheta**2*k1*r**2*(dr+2*r)/(dtheta**2*r**2+dr**2)/(-dr*k1+dr*k2+2*r*k1+2*r*k2),0]]
                         )
 
+    return kernel, (1,1,1,1), ""
+
+def eq_temp(key, dr, dtheta, r, k1, k2):
     k = k2 if str(key)[-1] == '1' else k1
     scaler = dtheta**2*dr**2*r**2/(2*(dtheta**2*r**2+dr**2))/k
+
+    kernel = np.array([[0,dtheta**2*r*(-dr+2*r)/(4*(dtheta**2*r**2+dr**2)),0],
+                        [dr**2/(2*(dtheta**2*r**2+dr**2)),0,dr**2/(2*(dtheta**2*r**2+dr**2))],
+                        [0,dtheta**2*r*(dr+2*r)/(4*(dtheta**2*r**2+dr**2)),0]]
+                        )
+
+    if key == 21 or key == 20:
+        kernel = np.array([[0,dtheta**2*r*(-dr+2*r)/4/(dtheta**2*r**2+dr**2),0],
+                            [dr**2/(dtheta**2*r**2+dr**2),0,0],
+                            [0,dtheta**2*r*(dr+2*r)/4/(dtheta**2*r**2+dr**2),0]])
+    if key == 31 or key == 30:
+        kernel = np.array([[0,dtheta**2*k1*r**2*(-dr+2*r)/(dtheta**2*r**2+dr**2)/(-dr*k1+dr*k2+2*r*k1+2*r*k2),0],
+                            [dr**2/(2*(dtheta**2*r**2+dr**2)),0,dr**2/(2*(dtheta**2*r**2+dr**2))],
+                            [0,dtheta**2*k2*r**2*(dr+2*r)/(dtheta**2*r**2+dr**2)/(-dr*k1+dr*k2+2*r*k1+2*r*k2),0]]
+                        )
+    if key == 41 or key == 40:
+        kernel = np.array([[0,dtheta**2*r*(-dr+2*r)/(4*(dtheta**2*r**2+dr**2)),0],
+                        [dr**2*k2/(k1+k2)/(dtheta**2*r**2+dr**2),0,dr**2*k1/(k1+k2)/(dtheta**2*r**2+dr**2)],
+                        [0,dtheta**2*r*(dr+2*r)/(4*(dtheta**2*r**2+dr**2)),0]]
+                        )
+    if key == 51 or key == 50:
+        kernel = np.array([[0,dtheta**2*k2*r**2*(-dr+2*r)/(dtheta**2*r**2+dr**2)/(-dr*k1+dr*k2+2*r*k1+2*r*k2),0],
+                        [dr**2/(2*(dtheta**2*r**2+dr**2)),0,dr**2/(2*(dtheta**2*r**2+dr**2))],
+                        [0,dtheta**2*k1*r**2*(dr+2*r)/(dtheta**2*r**2+dr**2)/(-dr*k1+dr*k2+2*r*k1+2*r*k2),0]]
+                        )
+
+    if key == 10 or key == 11:
+        kernel = np.array([[0,0,0],[0,1,0],[0,0,0]])
+
+    if key == 60 or key == 61:
+        h = 50
+        denominator = dtheta**2*dr**2*h*r + 2*dtheta**2*dr*h*r**2 + 2*dtheta**2*k*r**2 + 2*dr**2*k
+        kernel = np.array([[0,2*dtheta**2*k*r**2/denominator,0],
+                          [dr**2*k/denominator,0,dr**2*k/denominator],
+                          [0,dtheta**2*dr*h*r*(dr+2*r)/denominator,0]])
+        scaler =  dtheta**2*dr**2*r**2/denominator
+
+    if key == 600 or key == 610:
+        h = 50
+        denominator = dtheta**2*dr**2*h*r + 2*dtheta**2*dr*h*r**2 + 2*dtheta**2*k*r**2 + 2*dr**2*k
+        kernel = np.array([[0,2*dtheta**2*k*r**2/denominator,0],
+                          [2*dr**2*k/denominator,0,0],
+                          [0,dtheta**2*dr*h*r*(dr+2*r)/denominator,0]])
+        scaler =  dtheta**2*dr**2*r**2/denominator
+
     return kernel, (1,1,1,1), scaler
 
 def eq_r(key, dr, dp, r, k1, k2):
@@ -54,7 +102,7 @@ def eq_r(key, dr, dp, r, k1, k2):
                                [0, -1/(2*dr),0]])
         size = (0,2,1,1)
         
-    if key == 31 or key == 30:
+    if key == 61 or key == 60:
         kernel = -k*np.array([ [0,  1/(2*dr),0],
                                [0, -4/(2*dr),0],
                                [0,  3/(2*dr),0]])
@@ -77,7 +125,7 @@ def eq_p(key, dr, dp, r, k1, k2):
     kernel = -k*np.array([ [0,          0, 0],
                         [-1/(2*dp*r),0,1/(2*dp*r)],
                         [0,          0, 0]])
-    if key == 41 or key == 40:
+    if key == 21 or key == 20:
         size = (1,1,2,0)
         kernel = -k*np.array([ [0,          0, 0],
                             [1/(2*dp*r),-4/(2*dp*r),3/(2*dp*r)],
@@ -95,8 +143,8 @@ if __name__ == '__main__':
     # argumento da linha de comando
     arg1 = sys.argv[-1]
 
-    r1,r2,R1,R2,t,T,ka,kb,v0 = 0.05, 0.08, 0.03, 0.11, np.deg2rad(18), np.deg2rad(40), 5e6, 1e5, 50
-    dr,dp = (R2-R1)/80, T/40
+    r1,r2,R1,R2,t,T,ka,kb,v0 = 0.05, 0.08, 0.03, 0.11, np.deg2rad(18), np.deg2rad(40), 5e-6, 1e-5, 50
+    dr,dp = (R2-R1)/40, T/24
 
     if arg1.lower() == '-y':
         mesh_0 = Mesh(r1,r2,R1,R2,t,T,ka,kb,v0,dr,dp,eq)
@@ -126,7 +174,7 @@ if __name__ == '__main__':
     Z = (-(Jr**2 + Jp**2)/k)
     np.savetxt('qponto.txt',Z)
 
-    mesh_3 = Mesh(r1,r2,R1,R2,t,T,110,500,v0,dr,dp,eq, consts=(30,25))
-    mesh_3.run(add=-Z)
+    mesh_3 = Mesh(r1,r2,R1,R2,t,T,110,500,25+273,dr,dp,eq_temp, consts=(30+273,25+273))
+    mesh_3.run(add = -Z, immutable=False)
     T = mesh_3.V
     np.savetxt('T.txt',T)
